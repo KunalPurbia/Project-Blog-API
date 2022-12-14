74b84156083ab3557fb15b5da332eca69ec5d6cb
KunalPurbia <Kunal.Purbia@softobiz.com>
2022-12-14 17:20:00 +0530

    Update to author route test successful

--- server/__tests__/allRoutes.test.js
+++ server/__tests__/allRoutes.test.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 const config = require('../config/config');
 const request = require('supertest')
 
+//////////////////////////////////////////////////////////VARIABLES TO STORE TOKEN FOR AUTHENTICATION
+var viewerToken = "";
+var authorToken = "";
+
 //////////////////////////////////////////////////////////SETTING MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({
@@ -24,16 +28,16 @@ require('../middlewares/jwtAuth');
 //////////////////////////////////////////////////////////REQUIRING ROUTES
 const signinRouter = require('../routes/signinRouter');
 const loginRouter = require('../routes/loginRouter');
+const viewerRouter = require('../routes/viewerRouter');
 // const authorRouter = require('../routes/authorRouter');
-// const viewerRouter = require('../routes/viewerRouter');
 
 //////////////////////////////////////////////////////////SETTING ALL ROUTES
 app.use("/sign-in", signinRouter);
 app.use("/log-in", loginRouter);
+app.use("/viewer", viewerRouter);
 // app.use("/author", authorRouter);
-// app.use("/viewer", viewerRouter);
 
-//////////////////////////////////////////////////////////Sign In route testing
+//////////////////////////////////////////////////////////SIGN IN ROUTE TESTING
 describe("Sign In route test", () => {
     it('POST /sign-in on successfull sign in', async () => {
         const { body, statusCode } = await request(app).post("/sign-in").send({
@@ -72,7 +76,7 @@ describe("Sign In route test", () => {
     });
 });
 
-//////////////////////////////////////////////////////////Log In route testing
+//////////////////////////////////////////////////////////LOGIN ROUTE TESTING
 describe("Log in route test", () => {
     it('POST /log-in on successfull log in', async () => {
         const { body, statusCode } = await request(app).post("/log-in").send({
@@ -99,9 +103,34 @@ describe("Log in route test", () => {
     });
 });
 
-//////////////////////////////////////////////////////////DESTROYING DATABASE AFTER COMPLETE TEST
-afterAll((done) => {
-    mongoose.connection.db.dropDatabase(() => {
-        mongoose.connection.close(() => done())
+//////////////////////////////////////////////////////////VIEWER ROUTE TESTING TO MAKE AUTHOR
+describe("Viewer route test for update to Author", () => {
+
+    it('POST /log-in on successfull log in', async () => {
+        const { body, statusCode } = await request(app).post("/log-in").send({
+            email: "author@test.com",
+            password: "123"
+        });
+        viewerToken = "Bearer " + body[1];
+        expect(statusCode).toBe(200);
+    });
+
+    it('PUT /viewer/becomeAuthor - to make viewer Author for blog', async () => {
+        const { body, statusCode } = await request(app)
+            .put("/viewer/becomeAuthor")
+            .set("Authorization", viewerToken)
+            .send({
+                code: "dreamAuthor000"
+            });
+        expect(statusCode).toBe(200)
     });
 });
+
+//////////////////////////////////////////////////////////AUTHOR ROUTE TESTING FOR CRUD BLOG
+
+// //////////////////////////////////////////////////////////DESTROYING DATABASE AFTER COMPLETE TEST
+// afterAll((done) => {
+//     mongoose.connection.db.dropDatabase(() => {
+//         mongoose.connection.close(() => done())
+//     });
+// });
